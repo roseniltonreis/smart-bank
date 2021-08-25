c5a89e153a53dcde7a3c5ca1c3353c91ed60201e
Nill Reis <roseniltonreis@finchsolucoes.com.br>
2021-08-24 22:26:03 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Smart Bank
+
+## React: Abstraindo seu CSS com Styled Components
+
+Simples sistema desenvolvido em ReactJS com a lib Styled-components (https://styled-components.com) com base em projeto do curso de frontend da Alura
+
+Alguns dos pontos bases apresentados no curso foram:
+
+- Configuração de estilo global;
+- Criação/Estilização dos componentes;
+- Herança de componentes;
+- Alteração de tema claro/escuro;
+- Media queries;
+- Boas práticas com CSS;
---
+++ src/Components/Conta/index.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+import privado from "../../assets/images/privado.svg";
+import olho_icone from "../../assets/images/olho.svg";
+import dinheiro from "../../assets/images/dinheiro.svg";
+import { Box, Detalhe, Icone, Saldo, Botao, IconeTema } from "../UI";
+import styled from "styled-components";
+
+const IconeMargin = styled(Icone)`
+  margin-top: 2px;
+`;
+
+const Conta = () => {
+  const [toggleState, untoggle] = useState(true);
+
+  const toggleHandler = () => {
+    untoggle((toggleState) => !toggleState);
+  };
+
+  return (
+    <Box className="box">
+      <h2>Conta</h2>
+      <div style={{ fontSize: "26px", padding: "20px 0" }}>
+        Saldo disponível{" "}
+        <span>
+          <IconeTema src={dinheiro} alt="Ícone Saldo" />
+        </span>
+        {toggleState ? (
+          <Saldo>
+            <Detalhe className="detalhe">R$</Detalhe> 0,00{" "}
+          </Saldo>
+        ) : null}
+      </div>
+
+      <Botao className="btn" onClick={toggleHandler}>
+        <IconeMargin
+          className="imagem-icone"
+          src={toggleState ? privado : olho_icone}
+          alt="Privacidade do Saldo"
+        />
+      </Botao>
+    </Box>
+  );
+};
+
+export default Conta;
